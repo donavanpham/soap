c1b35fa5b06bd187ce694d1ef9a7648c066d88c3
Don <donavanpham@gmail.com>
2020-10-21 17:38:29 -0400

    added tabs

--- template.js
+++ template.js
@@ -19,10 +19,15 @@ function display(section, other){
     var other = document.getElementById(other).value;
     for (i=0;i<items.length; i++){
         if (items[i].checked === true){
-            str += items[i].value +"<br>"
+            str += items[i].value +"<br>";
         }
     }
-    document.getElementById(section).innerHTML = str + other + "<br>";
+    if (other === null ){
+        document.getElementById(section).innerHTML = str +"<br>";
+    }else{
+        document.getElementById(section).innerHTML = str + other;
+    }
+    
 };
 
 // sections
@@ -30,6 +35,8 @@ const sections = {
     "HPI":"otherHPI",
     "location":"otherLocation",
     "radiation":"otherRadiation",
+    "quality":"otherQuality",
+    "timing":"otherTiming",
     "exam": "otherExam",
     "ROS": "otherROS",
     "plan":"otherPlan"
@@ -58,4 +65,26 @@ function CopyToClipboard(id) {
     window.getSelection().removeAllRanges();
 }
 
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+openCity(event, 'London');
 
