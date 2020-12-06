46b1ef4cee517d214c1ca8b938239ef637ac9b85
Don <donavanpham@gmail.com>
2020-12-06 12:12:04 -0500

    added headache

--- general.js
+++ general.js
@@ -1,10 +1,10 @@
 //checkbox clicking
 function attachCheckboxHandlers() {
-	var el = document.getElementById('allCheckbox');
-	var tops = el.getElementsByTagName('input');
+	var el = document.getElementById("main");
+	var tops = el.getElementsByTagName("input");
 
 	for (var i = 0, len = tops.length; i < len; i++) {
-		if (tops[i].type === 'checkbox' || 'text' || 'number') {
+		if (tops[i].type === "checkbox" || "text" || "number") {
 			tops[i].onclick = displayAll;
 			tops[i].oninput = displayAll;
 		}
@@ -14,14 +14,14 @@ function attachCheckboxHandlers() {
 // display
 function display(section, other) {
 	var items = document.getElementsByClassName(section);
-	var str = '';
+	var str = "";
 	var other = document.getElementById(other).value;
-	if (other !== '') {
-		other += '<br>';
+	if (other !== "") {
+		other += "<br>";
 	}
 	for (i = 0; i < items.length; i++) {
 		if (items[i].checked === true) {
-			str += items[i].value + '<br>';
+			str += items[i].value + "<br>";
 		}
 	}
 
@@ -30,17 +30,18 @@ function display(section, other) {
 
 // sections
 const sections = {
-	HPI         : 'otherHPI',
-	location    : 'otherLocation',
-	radiation   : 'otherRadiation',
-	quality     : 'otherQuality',
-	timing      : 'otherTiming',
-	aggravation : 'otherAggravation',
-	relief      : 'otherRelief',
+	HPI         : "otherHPI",
+	location    : "otherLocation",
+	radiation   : "otherRadiation",
+	quality     : "otherQuality",
+	timing      : "otherTiming",
+	aggravation : "otherAggravation",
+	relief      : "otherRelief",
 
-	exam        : 'otherExam',
-	ROS         : 'otherROS',
-	plan        : 'otherPlan'
+	exam        : "otherExam",
+	ROS         : "otherROS",
+	assessment  : "otherAssessment",
+	plan        : "otherPlan"
 };
 const entries = Object.entries(sections);
 
@@ -59,33 +60,33 @@ function CopyToClipboard(id) {
 	r.selectNode(document.getElementById(id));
 	window.getSelection().removeAllRanges();
 	window.getSelection().addRange(r);
-	document.execCommand('copy');
+	document.execCommand("copy");
 	window.getSelection().removeAllRanges();
 
-	var tooltip = document.getElementById('myTooltip');
-	tooltip.innerHTML = 'Copied';
+	var tooltip = document.getElementById("myTooltip");
+	tooltip.innerHTML = "Copied";
 }
 
 function outFunc() {
-	var tooltip = document.getElementById('myTooltip');
-	tooltip.innerHTML = 'Copy to clipboard';
+	var tooltip = document.getElementById("myTooltip");
+	tooltip.innerHTML = "Copy to clipboard";
 }
 
 function openSection(evt, sectionName) {
 	// Declare all variables
 	var i, tabcontent, tablinks;
 	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName('tabcontent');
+	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = 'none';
+		tabcontent[i].style.display = "none";
 	}
 	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName('tablinks');
+	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(' active', '');
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 	// Show the current tab, and add an "active" class to the link that opened the tab
-	document.getElementById(sectionName).style.display = 'block';
-	evt.currentTarget.className += ' active';
+	document.getElementById(sectionName).style.display = "grid";
+	evt.currentTarget.className += " active";
 }
-document.getElementById('defaultOpen').click();
+document.getElementById("defaultOpen").click();
